4dc823145a2b6d1f5be093af6e0d4a2ca3a83764
Elok Desktop <bradencbateman@gmail.com>
2025-06-20 14:36:23 -0600

    move all files into root driectory (hopefully fixes github pages)

---
+++ Sprites.js
@@ -0,0 +1,9 @@
+export const playerSprites = [
+    './testPlayerSpritesGray.png',
+    './testPlayerSpritesRed.png',
+    './testPlayerSpritesYellow.png',
+    './testPlayerSpritesGreen.png',
+    './testPlayerSpritesBlue.png',
+    './testPlayerSpritesPurple.png',
+    './testPlayerSpritesPink.png'
+]
--- scripts/Sprites.js
+++
@@ -1,9 +0,0 @@
-export const playerSprites = [
-    '/assets/testPlayerSpritesGray.png',
-    '/assets/testPlayerSpritesRed.png',
-    '/assets/testPlayerSpritesYellow.png',
-    '/assets/testPlayerSpritesGreen.png',
-    '/assets/testPlayerSpritesBlue.png',
-    '/assets/testPlayerSpritesPurple.png',
-    '/assets/testPlayerSpritesPink.png'
-]
